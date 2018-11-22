fb70a30a1b28039f55db9b875cc9bc8b6b41b506
BSzydlowska <40805967+BSzydlowska@users.noreply.github.com>
2018-11-22 14:02:33 +0100

    Add files via upload

---
+++ src/com/lulv2code/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,9 @@
+package com.lulv2code.springdemo.dao;
+
+import java.util.List;
+
+import com.luv2code.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	public List<Customer> getCustomers();
+}
